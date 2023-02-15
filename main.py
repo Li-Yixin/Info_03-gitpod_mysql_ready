ad5c87b83c771bcd10e22fbcf3a612d1db70a49b
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2023-02-15 11:59:39 +0000

    V.1

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password=""
+  database="mydatabase"
 )
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor()   #crea in python un cursore#
 
 mycursor.execute("CREATE DATABASE mydatabase")
